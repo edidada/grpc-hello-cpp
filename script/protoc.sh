08a2dd5e3fbcd6d575b3d5d3b69d98457e8dd0aa
edidada <sandisks555@gmail.com>
2021-02-10 19:57:02 +0800

    use vcpkg in mac success 20210210

--- script/protoc.sh
+++ script/protoc.sh
@@ -1,2 +1,2 @@
 protoc -I=../protos --cpp_out=../protos helloworld.proto
-protoc -I=../protos --grpc_out=../protos --plugin=protoc-gen-grpc=/home/travis/build/edidada/vcpkg/packages/grpc_x64-osx/tools/grpc/grpc_cpp_plugin helloworld.proto
+protoc -I=../protos --grpc_out=../protos --plugin=protoc-gen-grpc=/home/travis/build/edidada/vcpkg/packages/grpc_x64-linux/tools/grpc/grpc_cpp_plugin helloworld.proto
