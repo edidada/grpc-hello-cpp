e272aaf761a6ea2e86db2727ae0e38c90ef2eece
edidada <sandisks555@gmail.com>
2021-02-11 12:08:57 +0800

    use vcpkg in mac success 20210211

--- script/protoc.sh
+++ script/protoc.sh
@@ -1,2 +1,6 @@
-/root/.local/bin/protoc -I=../protos --cpp_out=../protos helloworld.proto
+find / -name "protoc"
+find / -name "grpc_cpp_plugin"
+sudo find / -name "protoc"
+sudo find / -name "grpc_cpp_plugin"
+protoc -I=../protos --cpp_out=../protos helloworld.proto
 protoc --grpc_out=../protos --plugin=protoc-gen-grpc=/root/grpc_1.15.1_withoutgit/cmake/build/grpc_cpp_plugin helloworld.proto
